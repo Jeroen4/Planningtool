94061ff4e12bae444a24b570cc63f5e1f6f886c8
JeroenFaasse <99045518@mydavinci.nl>
2019-04-02 10:17:16 +0200

    added games and created DB connection

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+	include 'connect.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Planningstool</title>
+</head>
+<body>
+	<form autocomplete="off">
+		<input type="text" name="name" placeholder="naam...">
+		<input type="text" name="age" placeholder="leeftijd...">
+		<input type="text" name="game" placeholder="kies een spel...">
+		<input type="text" name="leader" placeholder="naam van uitlegger...">
+		<input type="text" name="starttime" placeholder="starttijd">
+		<input type="text" name="players" placeholder="namen van spelers...">
+	</form>
+</body>
+</html>
