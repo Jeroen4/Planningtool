ca92f549c251affeaa3a37283c26ed90c1c03c8f
JeroenFaasse <99045518@mydavinci.nl>
2019-05-07 14:10:13 +0200

    bug in planner

--- datalayer.php
+++ datalayer.php
@@ -2,7 +2,6 @@
 function getGames(){
 	include ('connect.php');
 
-
 	$query = $conn->prepare("SELECT * FROM games");
 		$query->execute();
 		$result = $query->fetchAll();
@@ -16,6 +15,5 @@ function getGames(){
         $stmt = $conn->prepare("INSERT INTO planning (spel, spelers, spelleider, starttijd, datum) VALUES (:spel, :spelers, :leider, :starttijd, :datum)");
         $stmt->execute([':spel' => $_POST['spel'], ':spelers' => $_POST['spelers'], ':leider' => $_POST['leider'],  ':starttijd' => $_POST['starttijd'], ':datum' => $_POST['datum']]);
         $conn = null;
-
     }
 ?>
